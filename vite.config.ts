e65e64ac42d864dd740147f0d9c6edf7f25415f0
Ausath Ikram <ausathdzil@gmail.com>
2026-04-29 15:58:31 +0700

    chore: update packages, editor settings, and formatter config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from "vite-plus";
-import { devtools } from "@tanstack/devtools-vite";
-
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
-import viteReact from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
+import tailwindcss from '@tailwindcss/vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
+import { defineConfig } from 'vite-plus'
 
 const config = defineConfig({
   staged: {
-    "*": "vp check --fix",
+    '*': 'vp check --fix',
   },
-  fmt: {},
+  fmt: {
+    singleQuote: true,
+    semi: false,
+    sortImports: true,
+  },
   lint: { options: { typeAware: true, typeCheck: true } },
   resolve: { tsconfigPaths: true },
   plugins: [
@@ -21,6 +23,6 @@ const config = defineConfig({
     tanstackStart(),
     viteReact(),
   ],
-});
+})
 
-export default config;
+export default config
